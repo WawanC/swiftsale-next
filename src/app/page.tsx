bb7e3f92d2a1a1c9405617a9dbd3a0ad49951131
Kurniawan Cristianto <kurniawancristianto@gmail.com>
2023-10-04 21:33:17 +0700

    bug test: add test link temporarily

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { getProductsApiServer } from "@/api/server/product";
 import ProductList from "@/app/_components/ProductList";
 import NavBar from "@/app/_components/NavBar";
+import Link from "next/link";
 
 const Home = async () => {
   // try {
@@ -15,6 +16,11 @@ const Home = async () => {
     <>
       <NavBar />
       <main className={`flex-1 flex flex-col items-center py-8`}>
+        <div
+          className={`flex justify-center items-center p-8 text-xl underline underline-offset-8`}
+        >
+          <Link href={"/test"}>Go to test</Link>
+        </div>
         <ProductList products={products} />
       </main>
     </>
