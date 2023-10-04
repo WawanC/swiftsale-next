51697274c13ce090f49a9ec96f1aa5c3d24d5456
Kurniawan Cristianto <kurniawancristianto@gmail.com>
2023-10-04 21:39:33 +0700

    bug test: remove api middleware temporarily

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export const config = {
-  matcher: ["/api/:path*"],
+  matcher: ["/xxx/:path*"],
 };
 
-export function middleware(request: NextRequest) {
-  return NextResponse.rewrite(
-    new URL(
-      `${process.env.PROXY_URL}${request.nextUrl.pathname}${request.nextUrl.search}`,
-    ),
-    { request },
-  );
+export function middleware(_: NextRequest) {
+  // return NextResponse.rewrite(
+  //   new URL(
+  //     `${process.env.PROXY_URL}${request.nextUrl.pathname}${request.nextUrl.search}`,
+  //   ),
+  //   { request },
+  // );
+  return NextResponse.next();
 }
